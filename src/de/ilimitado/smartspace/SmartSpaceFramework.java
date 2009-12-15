030db53745a85f1f733186ffe3bc286c4d9585c8
otw <otw@otw-desktop.(none)>
2009-12-15 13:46:59 +0100

    SmartSpaceFrame now extends Service instead of Thread, implemented onStart()...

--- src/de/ilimitado/smartspace/SmartSpaceFramework.java
+++ src/de/ilimitado/smartspace/SmartSpaceFramework.java
@@ -2,8 +2,11 @@ package de.ilimitado.smartspace;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.IBinder;
 import android.os.PowerManager;
 import de.ilimitado.smartspace.android.AndroidConfigTranslator;
 import de.ilimitado.smartspace.fsm.FSM;
@@ -11,43 +14,38 @@ import de.ilimitado.smartspace.fsm.InitialState;
 import de.ilimitado.smartspace.persistance.PersistanceManager;
 import de.ilimitado.smartspace.registry.Registry;
 
-public final class SmartSpaceFramework extends Thread{
+public final class SmartSpaceFramework extends Service{
 	
 	public final static int INDOOR_POSITION_PROVIDER = 0;
 	
 	private static IndoorPositionManager iPosMngr;
 	private static FSM appStateMachine;
-	private Context androidCtx;
-	private boolean stayAlive = true;
 	
-	public SmartSpaceFramework(Context androidCtx) {
-		super("SmartSpaceMain");
-		this.androidCtx = androidCtx;
-	}
 	@Override
-	public void run() {
+	public void onStart(Intent intent, int startId) {
+		super.onStart(intent, startId);
 		try {
 			bootstrap();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		while(stayAlive) {
-			//have Fun
-		}
-		//!stayAlive ---> RIP
-		interrupt();
 	}
 	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	public void bootstrap() throws Exception {
-		SharedPreferences androidPreferences = androidCtx.getSharedPreferences("smartSpace", 1);
+		SharedPreferences androidPreferences = this.getSharedPreferences("smartSpace", 1);
 		AndroidConfigTranslator.getInstance(androidPreferences).translate();
 		LinkedBlockingQueue<SensorEvent<?>> systemRawDataQueue = new LinkedBlockingQueue<SensorEvent<?>>();
 		MotionDetector mtnDet = new MotionDetector();
 		Registry reg = new Registry();
 		PersistanceManager persMngr = new PersistanceManager();
 		SensorManager sMng = new SensorManager();
-		final PowerManager pm = (PowerManager) androidCtx.getSystemService(Context.POWER_SERVICE);
+		final PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
 		sMng.setPowerManager(pm);
 		PositionManager posMngr = new PositionManager();
 		IndoorPositionManager iPosMngr= new IndoorPositionManager(posMngr, sMng);
@@ -55,7 +53,7 @@ public final class SmartSpaceFramework extends Thread{
 		EventSynchronizer evtSync = new EventSynchronizer();
 		SensingReactor sReact = new SensingReactor(reg, systemRawDataQueue);
 		SensorDependencies sDep = new SensorDependencies(sReact, evtSync, sMng, systemRawDataQueue, reg);
-		Dependencies appDep = new Dependencies(androidCtx, sDep, mtnDet, persMngr, posMngr);
+		Dependencies appDep = new Dependencies(this, sDep, mtnDet, persMngr, posMngr);
 		SensorLoader sensorLoader = new SensorLoader(appDep);
 		sensorLoader.loadSensors();
 		appDep.sensorDependencies.sensorManager.initSensors();
@@ -67,7 +65,7 @@ public final class SmartSpaceFramework extends Thread{
 	}
 	
 	public void die(){
-		stayAlive = false;
+//		stayAlive = false;
 	}
 	
 	public static IndoorPositionManager getSystemService(int service){
